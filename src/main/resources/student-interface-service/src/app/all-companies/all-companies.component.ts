db92f192b8f3184dd88fd28e65ef9ab5698c5ca6
Yacer <mryaser8@gmail.com>
2021-05-04 00:43:03 +0100

    updated company tiles and added paginator

--- src/main/resources/student-interface-service/src/app/all-companies/all-companies.component.ts
+++ src/main/resources/student-interface-service/src/app/all-companies/all-companies.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {CompanyService} from "../service/company.service";
 import {CompanyModel} from "../model/company.model";
 import {Router} from "@angular/router";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-all-companies',
@@ -11,27 +14,42 @@ import {Router} from "@angular/router";
 export class AllCompaniesComponent implements OnInit {
   loading = true;
   companies: CompanyModel[] = [];
+  datasource: MatTableDataSource<CompanyModel>;
+  obs: Observable<any>;
+  private paginator: MatPaginator;
 
-  constructor(private companyService: CompanyService, private router: Router) {
-    this.companyService.getAllCompanies().subscribe(data => {
-      for(let company of data) {
-        this.getRating(company);
-      }
-      this.loading = false;
-    });
+  @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.setDataSourceAttributes();
+  }
+
+  setDataSourceAttributes() {
+    this.datasource.paginator = this.paginator;
+  }
+
+  constructor(private companyService: CompanyService, private router: Router,
+              private changeDetectorRef: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
+    this.companyService.getAllCompanies().subscribe(data => {
+      this.datasource = new MatTableDataSource<CompanyModel>(this.getRating(data));
+      this.changeDetectorRef.detectChanges();
+      this.obs = this.datasource.connect();
+      this.loading = false;
+    });
   }
 
   goToCompanyProfile(name: string) {
     this.router.navigateByUrl('/company/' + name);
   }
 
-  getRating(company: CompanyModel): void {
-    this.companyService.getRating(company.name).subscribe(data => {
-      company.profile.rating = Number((Math.round(data * 100) / 100).toFixed(2));
-      this.companies.push(company);
-    });
+  getRating(companies: CompanyModel[]): any {
+    for(let company of companies) {
+      this.companyService.getRating(company.name).subscribe(data => {
+        company.profile.rating = Number((Math.round(data * 100) / 100).toFixed(2));
+      });
+    }
+    return companies;
   }
 }
